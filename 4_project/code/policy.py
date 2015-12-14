ab735c39ae5edb7668581b17fbc14dd5d6cd3d69
Thomas Rubeli <thomas@debian>
2015-12-14 12:29:02 +0100

    untested linUCB

--- 4_project/code/policy.py
+++ 4_project/code/policy.py
@@ -1,11 +1,42 @@
-import numpy.random
+import numpy
+
+articles_dic = {}
+user_feat = numpy.zeros(6)
+maxID = 0
 
 def set_articles(articles):
     pass
 
 def update(reward):
-    pass
+    articles_dic[maxID][0] += user_feat * user_feat.T
+    articles_dic[maxID][1] += reward*user_feat
 
 def recommend(time, user_features, articles):
-    return numpy.random.choice(articles, size=1)
 
+    maxUCB = None
+    user_feat = user_features
+    for id in articles:
+
+        # create or retrieve an entry in the dictionary
+        (M_x,b_x) = addToDic(id)
+
+        # initialize weights
+        M_x_inv = numpy.linalg.inv(M_x)
+        w_t = M_x_inv * b_x
+
+        #set UCBx
+
+        UCBx = numpy.dot(w_t,user_features) + alpha*numpy.sqrt(numpy.transpose(user_features)*M_x_inv*user_features)
+
+        if maxUCB is None or UCBx > maxUCB:
+            maxUCB = UCBx
+            maxID = id
+
+    return maxID
+
+def addToDic(id):
+    if id in articles_dic:
+        return articles_dic[id]
+    else:
+        articles_dic[id] = (numpy.identity(6),numpy.zeros(6))
+        return articles_dic[id]
