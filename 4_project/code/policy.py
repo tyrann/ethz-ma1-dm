5dbda0cc96b3bd71980c715c41f6a3746e6124b8
Marcel Marti <marmarti@student.ethz.ch>
2015-12-21 08:08:45 +0100

    Finished implementation.
    - Not yet tested.
    - Might run very slowly.
    - Might even be wrong.

--- 4_project/code/policy.py
+++ 4_project/code/policy.py
@@ -45,12 +45,13 @@ def tracking_matrix():
 A0 = feature_matrix();  # Hybrid linucb learned variable.
 B0 = feature_vector();  # Hybrid linucb learned variable.
 F  = feature_vector();  # The last user feature vector.
+M  = None               # The index of the maximum article.
 
 # We use a lookup table that stores key-value pairs where the key is the id of
 # an article and the value is a tuple (A, B, b) which keeps track of the article
 # related A and B matrices as well as the article related feature vector.
-H  = {}
-A  = []
+Lookup    = {}
+Articles  = []
 
 def set_articles(articles):
     """
@@ -66,12 +67,41 @@ def set_articles(articles):
     global A
     A = articles
 
-def update(reward):
-    # Index 0: Mx
-    # Index 1: bx
 
-    lookup[maxID][0] += np.outer(features, features)
-    lookup[maxID][1] += np.multiply(reward,features)
+def update(reward):
+    global Lookup
+    global A0
+    global B0
+
+    # Get the current data for the article.
+    data = select(M)
+    A = data[0]
+    I = data[1]
+    B = data[2]
+    b = data[3]
+
+    # Get the feature vector for the article.
+    x = Articles[M]
+
+    # Do initial update step for our global A0 and B0.
+    A0 = A0 + np.dot(B, np.dot(I, B))
+    B0 = B0 + np.dot(B, np.dot(I, b))
+
+    # Compute new data for the article.
+    newA = A + np.dot(x, x.T)
+    newI = np.linalg.inv(newA)
+    newB = B + np.dot(x, F.T)
+    newb = b + reward*x
+
+    # Do final update step for the article.
+    A0 = A0 + np.dot(F, F.T) - np.dot(newB.T, np.dot(newI, newB))
+    B0 = B0 + reward*F  np.dot(newB, np.dot(newI, newb))
+
+    # Update the data for the article.
+    Lookup[M][0] = newA
+    Lookup[M][1] = newI
+    Lookup[M][2] = newB
+    Lookup[M][3] = newb
 
 
 def reccomend(t, f, articles):
@@ -100,14 +130,47 @@ def reccomend(t, f, articles):
     global M    # Need to keep track of the selected article.
     global F    # Need to keep track of the user feature vector.
     F = f       # Update the user feature vector.
+    
+    # Initialize Beta and A0
+    A0inv = np.lianlg.inv(A0)
+    beta  = np.dot(A0inv, B0)
 
-    max_ucb = None
+    # Initialize local tracking values.
+    maxV = None # Maximum hybrid UCB value of article.
+    maxI = None # Id of article with maximum UCB value.
+
     for article in articles:
-
-        # create or retrieve an entry in the dictionary
-        Ainv, B, b = select(article)
         
-    return maxID
+        data = select(article)
+        A = data[0] 
+        I = data[1] 
+        B = data[2] 
+        b = data[3]
+
+        # Get article feature vector and initialize theta.
+        x = Articles[article]
+        t = b - np.dot(B, beta)
+        t = np.dot(I, t)
+
+        # Compute hybrid UCB value.
+        s1  = np.dot(f.T, np.dot(A0inv, f))
+        s21 = np.dot(B, np.dot(I, x))
+        s2  = 2*np.dot(f, np.dot(A0inv, s21))
+        s3  = np.dot(x, np.dot(I, x))
+        s41 = np.dot(B, np.dot(I, x))
+        s42 = np.dot(B, np.dot(A0inv, s41))
+        s4  = np.dot(x, np.dot(I, s42))
+        s   = s1 - s2 + s3 + s4
+
+        p = np.dot(f, beta) + np.dot(x, t) + alpha*np.sqrt(s)
+
+        if p > maxV:
+            maxV = p
+            maxI = article
+
+    M = maxI
+    return maxI
+
 
 def select(article):
     """
@@ -125,7 +188,8 @@ def select(article):
 
     A list containing the A, B and b values in that order.
     """
-    return H.setdefault(article, [
+    return Lookup.setdefault(article, [
+            feature_matrix(),
             feature_matrix(), 
             tracking_matrix(),
             feature_vector()
